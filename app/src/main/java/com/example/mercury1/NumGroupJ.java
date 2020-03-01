bebec06b3f9405f78ad6908cf8da8604d79c32b6
KC <keithc2chen@gmail.com>
2020-03-02 00:35:46 +0900

    fixing typo #2

---
+++ app/src/main/java/com/example/mercury1/NumGroupJ.java
@@ -0,0 +1,20 @@
+package com.example.mercury1;
+
+public class NumGroupJ {
+
+    public double allnumber;
+    public String ones = "0";           //１，１０の位
+    public String handreds = "0";       //１００の位
+    public String thousands = "0";      //千の位
+    public String one_m = "0";          //万の位
+    public String ten_m = "0";          //１０万
+    public String handreds_m = "0";     //１００万
+    public String thousands_m = "0";    //１千万
+    public String one_o = "0";          //１億
+    public String ten_o = "0";          //１０億
+    public String handred_o = "0";      //１００億
+    public String thousands_o = "0";    //１０００億
+    public String one_ch = "0";         //１兆
+}
+
+
